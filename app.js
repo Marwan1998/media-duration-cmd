270a53525828846e7bc32126dfde1ddf21635f0c
Marwan-Algadi <dragonkingofhell@gmail.com>
2022-05-13 05:34:10 +0200

    done with the code, ready to test

--- app.js
+++ app.js
@@ -29,8 +29,15 @@ async function welcome(){
     const rainbowAnimation = chalkanimation.rainbow('Hello, welcome to media duration package \n \tpowerd by Marwan Algadi');
     await sleep();
     rainbowAnimation.stop();
-    console.log(`${chalk.bgGray("\nProvide a correct path to calculate the video or audio files' time\n")}`);
+    console.log(`${chalk.bgGray("\nProvide a correct path to calculate the video or audio files' time \n")}`);
 }
+async function by(){
+  const rainbowAnimation = chalkanimation.rainbow('This package powerd by Marwan Algadi');
+  await sleep();
+  rainbowAnimation.stop();
+  console.log(chalk.bgRed(myBrand));
+}
+
 await welcome();
 
 async function getPath(){
@@ -44,51 +51,58 @@ async function getPath(){
     });
 
     const spinner = createSpinner('calculating...').start();
-    await sleep();
+    await sleep(1000);
     if(input.path != 'C:\\'){
         spinner.success({text: 'correct path'});
         return input.path;
     }else{
-        spinner.error({text: `wrong path provided, ${chalk.bgRed('exiting...')}`});
+        spinner.error({text: `please provide a valid path, ${chalk.bgRed('exiting...')}`});
         process.exit(1);
     }
 }
+
 path = await getPath();
-console.log(path);
 
+const pathName = path + "\\"
+const line = "\n-----------------------------------------------------------------";
 
-
-const pathName = "C:/Users/marwan/Desktop/editFolder/files/";
+//files calculation 
 const readDirPromise = new Promise((resolve, reject) => {
-  // console.time('start');
   let durSum = 0;
   fs.readdir(pathName, async (err, files) => {
     if (!err) {
       for (const file of files) {
         const fullPath = pathName + file;
-        await getVideoDurationInSeconds(fullPath).then((duration) => {
+        await getVideoDurationInSeconds(fullPath)
+        .then((duration) => {
           durSum += duration;
-          // console.log("durSum inside the methode: " + durSum);
+        })
+        .catch((videoErr) => {
+            console.log(chalk.bgRed(chalk.black("Video Error:\n")) + chalk.red("Non media files will be just ignored\n") + videoErr + chalk.green(line));
         });
       }
       resolve(durSum);
-      // console.timeEnd('start');
     } else {
-      console.error("Could not do it", err);
+      console.error(chalk.red("Could not do it!\nDirectory Error: it could be wrong path provided"));
       reject(err);
     }
   });
 });
 
+
 readDirPromise.then(durSum => {
   const time = {
+    //TODO: Add colors to the table
     Seconds: getMinutes(durSum).sec + 's',
     minutes: getMinutes(durSum).min + 'm',
     hours: getMinutes(durSum).hour + 'h',
   }
+  console.log(chalk.yellow(line));
   console.table(time);
+  console.log(chalk.yellow(line));
+  by();
 }).catch(err => {
-  console.log('something went wrong ', err);
+  console.log(err, chalk.green(line));
   process.exit(1);
 });
 
